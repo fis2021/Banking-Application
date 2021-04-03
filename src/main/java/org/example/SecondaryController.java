0a2f177a0e0a2e6e683dcb8214d9e13e5f1d4a3f
Mircea Feder <federmircea@gmail.com>
2021-04-03 23:23:39 +0300

    Test CSS

--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -6,7 +6,5 @@ import javafx.fxml.FXML;
 public class SecondaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
+    private void switchToPrimary() throws IOException { App.setRoot("primary"); }
 }
