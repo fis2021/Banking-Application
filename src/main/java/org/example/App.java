f772144018b465ee31411e0d4e0762fff1766984
Mircea Feder <federmircea@gmail.com>
2021-04-04 00:13:31 +0300

    Create UI design for login screen

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,14 +14,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("loginScreen"), 1280, 720);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+    static void setRoot(String fxml) throws IOException { scene.setRoot(loadFXML(fxml)); }
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
